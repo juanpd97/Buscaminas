9bb81a97579d88f8f0907d0ddab3990348133b1f
juan pablo dominguez <juanpdominguez97@gmail.com>
2026-01-15 20:40:22 -0300

    cambio en la forma de navegación entre pantallas

--- js/UI.js
+++ js/UI.js
@@ -1,97 +1,69 @@
-'use strict'
+"use strict";
 
+document.addEventListener("DOMContentLoaded", function () {
+  var btnJugar = document.getElementById("btn-jugar");
+  btnJugar.addEventListener("click", function () {
+    mostrarPantalla("pantalla-dificultad");
+  });
 
-var btnJugar = document.getElementById('btn-jugar');
-btnJugar.addEventListener('click', () => {
-    document.getElementById('pantalla-inicio').classList.remove('pantalla-activa');
-    document.getElementById('pantalla-dificultad').classList.add('pantalla-activa');
+  var btnVolverInicio = document.getElementById("btn-volver-inicio");
+  btnVolverInicio.addEventListener("click", function () {
+    mostrarPantalla("pantalla-inicio");
+  });
+
+  var btnRanking = document.getElementById("btn-ranking");
+  btnRanking.addEventListener("click", function () {
+    mostrarPantalla("pantalla-ranking");
+  });
+
+  var btnVolverInicioDesdeRanking = document.getElementById(
+    "btn-volver-inicio-desde-ranking"
+  );
+  btnVolverInicioDesdeRanking.addEventListener("click", function () {
+    mostrarPantalla("pantalla-inicio");
+  });
+
+  var btnFacil = document.getElementById("btn-facil");
+  btnFacil.addEventListener("click", function () {
+    mostrarPantalla("pantalla-juego");
+    empezarJuego(8, 8, 10);
+  });
+
+  var btnMedio = document.getElementById("btn-medio");
+  btnMedio.addEventListener("click", function () {
+    mostrarPantalla("pantalla-juego");
+    // empezarJuego(12, 12, 25);
+  });
+
+  var btnDificil = document.getElementById("btn-dificil");
+  btnDificil.addEventListener("click", function () {
+    mostrarPantalla("pantalla-juego");
+  });
+
+  var btnDificultadPersonalizada = document.getElementById(
+    "btn-dificultad-personalizada"
+  );
+  btnDificultadPersonalizada.addEventListener("click", function () {
+    mostrarPantalla("pantalla-personalizado");
+  });
+
+  var btnVolverDificultadDesdePersonalizado = document.getElementById(
+    "btn-volver-dificultad-desde-personalizado"
+  );
+  btnVolverDificultadDesdePersonalizado.addEventListener("click", function () {
+    mostrarPantalla("pantalla-dificultad");
+  });
 });
+// -----------------------------
 
-var btnVolverInicio = document.getElementById('btn-volver-inicio');
-btnVolverInicio.addEventListener('click', () => {
-    document.getElementById('pantalla-inicio').classList.add('pantalla-activa');
-    document.getElementById('pantalla-dificultad').classList.remove('pantalla-activa');
-});
+function mostrarPantalla(idPantalla) {
+  //i
+  var pantallas = document.getElementsByClassName("pantalla");
+  var i;
 
-var btnRanking = document.getElementById('btn-ranking');
-btnRanking.addEventListener('click', () => {
-    document.getElementById('pantalla-inicio').classList.remove('pantalla-activa');
-    document.getElementById('pantalla-ranking').classList.add('pantalla-activa');
-});
+  for (i = 0; i < pantallas.length; i++) {
+    pantallas[i].classList.remove("pantalla-activa");
+  }
 
-var btnVolverInicio = document.getElementById('btn-volver-inicio-desde-ranking');
-btnVolverInicio.addEventListener('click', () => {
-    document.getElementById('pantalla-inicio').classList.add('pantalla-activa');
-    document.getElementById('pantalla-ranking').classList.remove('pantalla-activa');
-});
-
-
-
-
-// Funciones para dibujar el tablero
-
-var btnFacil = document.getElementById('btn-facil');
-btnFacil.addEventListener('click', () => {
-    document.getElementById('pantalla-dificultad').classList.remove('pantalla-activa');
-    document.getElementById('pantalla-juego').classList.add('pantalla-activa');
-
-    //dibujarTablero('facil');
-});
-
-var btnMedio = document.getElementById('btn-medio');
-btnMedio.addEventListener('click', () => {
-    document.getElementById('pantalla-dificultad').classList.remove('pantalla-activa');
-    document.getElementById('pantalla-juego').classList.add('pantalla-activa');
-
-    //dibujarTablero('medio');
-});
-
-var btnDificil = document.getElementById('btn-dificil');
-btnDificil.addEventListener('click', () => {
-    document.getElementById('pantalla-dificultad').classList.remove('pantalla-activa');
-    document.getElementById('pantalla-juego').classList.add('pantalla-activa');
-
-    //dibujarTablero('dificil');
-});
-
-var btnDificultadPersonalizada = document.getElementById('btn-dificultad-personalizada');
-btnDificultadPersonalizada.addEventListener('click', () => {
-    document.getElementById('pantalla-dificultad').classList.remove('pantalla-activa');
-    document.getElementById('pantalla-personalizado').classList.add('pantalla-activa');
-});
-
-var btnVolverDificultadDesdePersonalizado = document.getElementById('btn-volver-dificultad-desde-personalizado');
-btnVolverDificultadDesdePersonalizado.addEventListener('click', () => {
-    document.getElementById('pantalla-dificultad').classList.add('pantalla-activa');
-    document.getElementById('pantalla-personalizado').classList.remove('pantalla-activa');
-});
-
-
-
-
-
-function dibujarTablero(dificultad) {
-    const tablero = document.getElementById('tablero');
-    tablero.innerHTML = ''; // Limpiar el tablero antes de dibujar
-
-    let filas, columnas;
-    if (dificultad === 'facil') {
-        filas = 8;
-        columnas = 8;
-    } else if (dificultad === 'medio') {
-        filas = 16;
-        columnas = 16;
-    } else if (dificultad === 'dificil') {
-        filas = 16;
-        columnas = 30;
-    }
-
-    tablero.style.gridTemplateRows = `repeat(${filas}, 1fr)`;
-    tablero.style.gridTemplateColumns = `repeat(${columnas}, 1fr)`;
-
-    for (let i = 0; i < filas * columnas; i++) {
-        const celda = document.createElement('div');
-        celda.classList.add('celda');
-        tablero.appendChild(celda);
-    }
-}   
+  document.getElementById(idPantalla).classList.add("pantalla-activa");
+}
--- js/logicaJuego.js
+++ js/logicaJuego.js
@@ -1,95 +1,87 @@
-'use strict'
+"use strict";
 
+function crearTablero(x, y) {
+  var tablero = [];
 
+  for (var i = 0; i < x; i++) {
+    tablero[i] = [];
+    for (var j = 0; j < y; j++) {
+      var casilla = {
+        tieneMina: false,
+        abierta: false,
+        bandera: false,
+        minasVecinas: 0,
+      };
 
-function crearTablero(x,y){
-
-    var tablero = [];
-
-    for(var i=0; i<x ;i++){
-        tablero[i]=[]
-        for(var j=0; j<y; j++){
-
-            var casilla = {
-                tieneMina: false,
-                abierta: false,
-                bandera: false,
-                minasVecinas: 0
-            };
-            
-            tablero[i][j] = casilla;
-        
-        }
+      tablero[i][j] = casilla;
     }
+  }
 
-    return tablero;
+  return tablero;
 }
 
 function colocarMina(cantMinas, tablero) {
-    var x, y;
-    var minasColocadas = 0;
+  var x, y;
+  var minasColocadas = 0;
 
-    while (minasColocadas < cantMinas) {
-        x = Math.floor(Math.random() * tablero.length);
-        y = Math.floor(Math.random() * tablero[0].length);
+  while (minasColocadas < cantMinas) {
+    x = Math.floor(Math.random() * tablero.length);
+    y = Math.floor(Math.random() * tablero[0].length);
 
-        if (tablero[x][y].tieneMina === false) {
-            tablero[x][y].tieneMina = true;
-            minasColocadas++;
-        }
+    if (tablero[x][y].tieneMina === false) {
+      tablero[x][y].tieneMina = true;
+      minasColocadas++;
     }
+  }
 }
-
 
 function minasVecinas(tablero, x, y) {
-    var contador = 0;
+  //i
+  var contador = 0;
 
+  for (var i = x - 1; i <= x + 1; i++) {
+    for (var j = y - 1; j <= y + 1; j++) {
+      if (i === x && j === y) {
+        continue;
+      }
+
+      if (tablero[i] === undefined || tablero[i][j] === undefined) {
+        continue;
+      }
+
+      if (tablero[i][j].tieneMina === true) {
+        contador++;
+      }
+    }
+  }
+
+  return contador;
+}
+
+function expandir(tablero, x, y) {
+  if (tablero[x][y].abierta === true) {
+    return;
+  }
+
+  if (tablero[x][y].cantMinas !== 0) {
+    tablero[x][y].abierta = true;
+  } else {
     for (var i = x - 1; i <= x + 1; i++) {
-        for (var j = y - 1; j <= y + 1; j++) {
-
-            if (i === x && j === y) {
-                continue;
-            }
-
-            if (tablero[i] === undefined || tablero[i][j] === undefined) {
-                continue;
-            }
-
-            if (tablero[i][j].tieneMina === true) {
-                contador++;
-            }
-        }
-    }
-
-    return contador;
-}
-
-function expandir(tablero,x,y){
-
-    if(tablero[x][y].abierta === true){
-        return;
-    }
-    
-    if (tablero[x][y].cantMinas !== 0) {
-        tablero[x][y].abierta = true;
-
-    } else {
-
-        for(var i= x-1 ; i<= x+1 ; i++){
-            for(var j = y-1; j<= y+1; j++){
-                if (i === x && j === y) {
-                    continue;
-                }
-
-                if (tablero[i] === undefined || tablero[i][j] === undefined) {
-                    continue;
-                } 
-
-                expandir(tablero,i,j);
-                
-            }
+      for (var j = y - 1; j <= y + 1; j++) {
+        if (i === x && j === y) {
+          continue;
         }
 
-    }   
+        if (tablero[i] === undefined || tablero[i][j] === undefined) {
+          continue;
+        }
 
+        expandir(tablero, i, j);
+      }
+    }
+  }
 }
+
+// function empezarJuego(x,y,cantMinas) {
+
+// }
